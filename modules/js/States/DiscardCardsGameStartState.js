c0fd77d78be6898ea73ce207209079c68eef96eb
Pavel Kulagin <kuzwiz@mail.ru>
2024-09-13 08:35:04 +0100

    [fix] do not make deck unselectable on selection in phase 0

--- modules/js/States/DiscardCardsGameStartState.js
+++ modules/js/States/DiscardCardsGameStartState.js
@@ -22,7 +22,7 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
                 if (selected.length === 2) {
                     this.clearPossible();
                     this.makeAllSelectedAndClickable(selected, this.selectLocation.bind(this));
-                    this.addUnselectableClass(dojo.query('.location:not(.selected)'));
+                    this.addUnselectableClass(dojo.query('#hand .location:not(.selected)'));
                     this.addPrimaryActionButton(
                         'buttonDiscardCards',
                         _('Discard selected'),
