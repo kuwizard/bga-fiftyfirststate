5b711e553b0aec70336c9e5e8c6cbac0372f4d7b
Pavel Kulagin <kuzwiz@mail.ru>
2024-09-18 22:45:09 +0200

    [feature] player resources are stuck to screen top when you scroll down

--- modules/js/playerboard.js
+++ modules/js/playerboard.js
@@ -1,7 +1,7 @@
 define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
     return declare('state.playerboard', null, {
         constructor() {
-            this._notifications.push(['resourcesChanged', 1]);
+            this.tick = false;
         },
 
         markPassed(players) {
@@ -15,7 +15,37 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
         addResourcesTable() {
             this.forEachPlayer((player) => {
                 dojo.place(this.format_block('jstpl_player_board', player), 'player_board_' + player.id);
+                if (player.id === this.player_id) {
+                    this.pinOrUnpinPlayerBoard();
+                    window.addEventListener(
+                        "scroll",
+                        () => {
+                            if (!this.tick) {
+                                setTimeout(() => {
+                                    this.pinOrUnpinPlayerBoard();
+                                    this.tick = false;
+                                }, 50)
+                            }
+                            this.tick = true;
+                        }
+                    );
+                }
             });
+        },
+
+        pinOrUnpinPlayerBoard() {
+            const playerResources = this.querySingle(`#overall_player_board_${this.player_id} .playerResourcesWrapper`);
+            if (playerResources.getBoundingClientRect().y < 0) {
+                if (!this.querySingle('#sticky')) {
+                    let newBoard = dojo.clone(playerResources);
+                    dojo.attr(newBoard, 'id', 'sticky');
+                    newBoard = dojo.place(newBoard, playerResources.parentNode)
+                    dojo.style(newBoard, 'width', `${playerResources.getBoundingClientRect().width}px`);
+                    dojo.style(newBoard, 'height', `${playerResources.getBoundingClientRect().height}px`);
+                }
+            } else {
+                dojo.destroy('sticky');
+            }
         },
 
         async notif_resourcesChanged(n) {
