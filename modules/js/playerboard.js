65f22988a2dfb44d97af7280370b2796ae3219f5
Pavel Kulagin <kuzwiz@mail.ru>
2024-09-25 22:22:29 +0100

    [fix] resources are not updating in the cloned player board + passed player board is not sticking to the top/not z-indexing forward

--- modules/js/playerboard.js
+++ modules/js/playerboard.js
@@ -17,13 +17,13 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
             this.forEachPlayer((player) => {
                 dojo.place(this.format_block('jstpl_player_board', player), 'player_board_' + player.id);
                 if (player.id === this.player_id) {
-                    this.pinOrUnpinPlayerBoard();
+                    this.pinOrUnpinPlayerBoard(player.passed);
                     window.addEventListener(
                         "scroll",
                         () => {
                             if (!this.tick) {
                                 setTimeout(() => {
-                                    this.pinOrUnpinPlayerBoard();
+                                    this.pinOrUnpinPlayerBoard(player.passed);
                                     this.tick = false;
                                 }, 50)
                             }
@@ -34,7 +34,7 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
             });
         },
 
-        pinOrUnpinPlayerBoard() {
+        pinOrUnpinPlayerBoard(isPassed) {
             const playerResources = this.querySingle(`#overall_player_board_${this.player_id} .playerResourcesWrapper`);
             if (playerResources.getBoundingClientRect().y < 0) {
                 if (!this.querySingle('#sticky')) {
@@ -43,8 +43,15 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
                     newBoard = dojo.place(newBoard, playerResources.parentNode)
                     dojo.style(newBoard, 'width', `${playerResources.getBoundingClientRect().width}px`);
                     dojo.style(newBoard, 'height', `${playerResources.getBoundingClientRect().height}px`);
+                    if (isPassed) {
+                        dojo.addClass('sticky', 'passed');
+                    }
+                    dojo.removeClass(this.querySingle(`#overall_player_board_${this.player_id}`), 'passed');
                 }
             } else {
+                if (isPassed) {
+                    dojo.addClass(this.querySingle(`#overall_player_board_${this.player_id}`), 'passed');
+                }
                 dojo.destroy('sticky');
             }
         },
@@ -58,6 +65,9 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
                     this.scoreCtrl[n.args.player_id].toValue(data[resource]);
                 } else {
                     this.querySingle(`#player_board_${n.args.player_id} .${resource}Value`).innerText = data[resource];
+                    if (n.args.player_id === this.player_id) {
+                        this.querySingle(`#sticky .${resource}Value`).innerText = data[resource];
+                    }
                 }
             });
         },
