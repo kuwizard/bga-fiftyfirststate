1dfcbfe587dce445afea978ebaaa8dc0e624845a
Pavel Kulagin <kuzwiz@mail.ru>
2024-09-25 14:29:14 +0100

    [fix] resources from storages should be returned during phase 2, not 4

--- modules/php/States/PhaseFourCleanupTrait.php
+++ modules/php/States/PhaseFourCleanupTrait.php
@@ -5,14 +5,10 @@ namespace STATE\States;
 use STATE\Core\Globals;
 use STATE\Core\Notifications;
 use STATE\Core\Stack;
-use STATE\Helpers\ResourcesHelper;
 use STATE\Managers\Connections;
 use STATE\Managers\Factions;
 use STATE\Managers\Locations;
 use STATE\Managers\Players;
-use STATE\Managers\Resources;
-use STATE\Models\FeatureStorageMultiple;
-use STATE\Models\Location;
 use STATE\Models\Player;
 
 trait PhaseFourCleanupTrait
@@ -25,23 +21,6 @@ trait PhaseFourCleanupTrait
                 Players::removeAllResources($player->getId());
                 Locations::resetActivatedTimes($player->getBoard()->getIds());
                 Notifications::playersResetAllResources();
-            }
-            foreach (Players::getAll() as $player) {
-                /** @var Location $location */
-                $storageLocations = $player->getBoard()->filter(function ($location) {
-                    return $location instanceof FeatureStorageMultiple;
-                });
-                /** @var FeatureStorageMultiple $location */
-                foreach ($storageLocations as $location) {
-                    $resources = $location->getResources();
-                    ResourcesHelper::increaseResourcesAfterAction($player, $resources);
-                    Resources::deleteAll($location->getId());
-                    Notifications::playerGotResourcesFromStorage(
-                        $player,
-                        $location->getId(),
-                        $player->getResourcesWithNames(array_unique($resources))
-                    );
-                }
             }
             Connections::discardFlippedEndOfRound();
             Players::resetAllPassed();
