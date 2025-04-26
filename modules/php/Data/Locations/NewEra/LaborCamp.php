ea59ad321a127a300e780d97b291f0c2d9b4c5d7
Pavel Kulagin <kuzwiz@mail.ru>
2025-04-26 11:27:13 +0100

    fixed Labour Camp icon

--- modules/php/Data/Locations/NewEra/LaborCamp.php
+++ modules/php/Data/Locations/NewEra/LaborCamp.php
@@ -14,7 +14,7 @@ class LaborCamp extends Action
         $this->name = clienttranslate("Labor Camp");
         $this->distance = 1;
         $this->spoils = [RESOURCE_WORKER, RESOURCE_WORKER];
-        $this->icons = [ICON_BRICK];
+        $this->icons = [ICON_WORKER];
         $this->deals = [RESOURCE_WORKER];
         $this->copies = 1;
         $this->action = new Act(
