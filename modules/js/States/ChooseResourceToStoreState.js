10cc41d20a668963898f92ecc69d484ef116a987
Pavel Kulagin <kuzwiz@mail.ru>
2026-04-08 22:43:40 +0100

    [169710] Reset turn" button showing out of turn

--- modules/js/States/ChooseResourceToStoreState.js
+++ modules/js/States/ChooseResourceToStoreState.js
@@ -6,19 +6,21 @@ define(['dojo', 'dojo/_base/declare'], (dojo, declare) => {
 
         onEnteringStateChooseResourceToStore(args) {
             debug('ChooseResourceToStore state', args);
-            setTimeout(() => { // Weird BGA bug, buttons do not appear
-                if (args._private) {
-                    args._private.forEach((resource) => {
-                        this.addActionButtonWithResource(resource, 'actChooseResourceToStore');
-                    });
-                    this.addPrimaryActionButton(
-                        'buttonActionPassStoring',
-                        _('Done'),
-                        () => this.takeAction('actPassStoringResource')
-                    );
-                }
-                this.addResetTurnButton()
-            }, 1);
+            if (this.isCurrentPlayerActive()) {
+                setTimeout(() => { // Weird BGA bug, buttons do not appear
+                    if (args._private) {
+                        args._private.forEach((resource) => {
+                            this.addActionButtonWithResource(resource, 'actChooseResourceToStore');
+                        });
+                        this.addPrimaryActionButton(
+                            'buttonActionPassStoring',
+                            _('Done'),
+                            () => this.takeAction('actPassStoringResource')
+                        );
+                    }
+                    this.addResetTurnButton()
+                }, 1);
+            }
         },
     });
 });
